3ced26c3f1799ad9caaea02af57fa3238f4cc521
Barun Pandey <barunn.pandey@gmail.com>
2022-04-05 08:57:46 +0545

    Initialize challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos] = useState([
+    "Add a button that allows users to add an item in todo list",
+    "Implement context",
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h4>Your assignment</h4>
+        <ul className="assignment-tasks">
+          <li>Add a button that allows users to add an item in to do list</li>
+          <li>
+            Add a button that allows users to remove an item in to do list
+          </li>
+          <li>
+            Separate todoitems listing component into a different component
+          </li>
+          <li>Bonus points if you can use Context to implement it</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Todos</h3>
+        <ul className="todos">
+          {todos.map((todoItem) => (
+            <li key={todoItem}>{todoItem}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
